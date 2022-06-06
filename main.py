f35f7834e5b23053b49c792a5c97133735c3f897
cburgyan <cburgyan@gmail.com>
2022-06-05 21:08:48 -0500

    create top 100 list in movie.txt

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
+
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
 
+html_text = requests.get(URL).text
 
+soup = bs4.BeautifulSoup(html_text, "html.parser")
+
+title_tags = soup.find_all(name="h3", class_="title")
+
+with open("movie.txt", "w", encoding="utf8") as file:
+    for tag in reversed(title_tags):
+        file.write(f"{tag.get_text()}\n")
+
+# OR
+# title_tags = soup.find_all(name="h3", class_="title")[::-1]
+#
+#
+# with open("movie.txt", "w", encoding="utf8") as file:
+#     for tag in title_tags:
+#         file.write(f"{tag.get_text()}\n")
